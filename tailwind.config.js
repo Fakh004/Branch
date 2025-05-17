bcf6cfc7ca09b7277b03889d89918e6f23c6e7c3
fakh <fakhridin008@gmail.com>
2025-05-17 15:00:42 +0300

    add html to pr

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {},
+      screens: {
+       mob:{max: '510px'}
+      // @media(max-width:510px) {}
+
+    }
+   },
+   plugins: [],
+ }
